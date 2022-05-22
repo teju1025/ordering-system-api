1649f6585ba3e4efdabbe35269b58e87d94f8362
teju1025 <s1092921@mail.ncyu.edu.tw>
2022-05-22 19:41:19 +0800

    05/22 修改時差與庫存

--- api/order.py
+++ api/order.py
@@ -24,7 +24,10 @@ class Order(Resource):
             orders = json.load(f)
         current_order_id = len(orders)
         orders[current_order_id] = {
-            '訂購時間': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            loc_dt = datetime.now()
+            time_del = datetime.timedelta(hours=8)
+            new_dt = loc_dt + time_del
+            '訂購時間': new_dt.strftime("%Y-%m-%d %H:%M:%S"),
             '訂購內容': order,
             '總金額': self._cacl_order_price(order)
         }
